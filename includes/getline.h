8d6c6ffa54c8ccafcdb402c3df97228dc98a7717
Barrett Kepler <barrett.kepler@gmail.com>
2020-09-10 19:46:38 +0300

    Norminette

--- includes/getline.h
+++ includes/getline.h
@@ -1,15 +1,17 @@
 #ifndef MINI_READER_GETLINE_H
-#define MINI_READER_GETLINE_H
+# define MINI_READER_GETLINE_H
 
-typedef struct s_getline
+typedef struct	s_getline
 {
-    int file_descriptor;
-    unsigned int buffer_size;
-    char *buffer;
-} t_getline;
+	int				file_descriptor;
+	unsigned int	buffer_size;
+	int				should_stop;
+	char			*buffer;
+	unsigned int	prev_line_size;
+}				t_getline;
 
-int getline_next(t_getline *getline, char **line);
-t_getline start_getline(char *filename, unsigned buffer_size);
-void stop_getline(t_getline *get_line);
+int				getline_next(t_getline *getline, char **line, int force_read);
+t_getline		start_getline(char *filename, unsigned buffer_size);
+void			stop_getline(t_getline *get_line);
 
-#endif //MINI_READER_GETLINE_H
+#endif
