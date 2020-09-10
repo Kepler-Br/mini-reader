8cb33f29fb3bc463388a0ecd0453d06958930a93
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2020-09-10 16:30:20 +0300

    Getline progress

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "getline.h"
-#include <malloc.h>
+#include <stdlib.h>
 
 int main()
 {
-    t_getline getline = start_getline("CMakeLists.txt", 1000);
-    char *line = getline_next(&getline);
+    t_getline getline = start_getline("CMakeLists.txt", 5);
+    char *line;
+    getline_next(&getline, &line);
     printf("%s\n", line);
     free(line);
-    line = getline_next(&getline);
+    getline_next(&getline, &line);
     printf("%s\n", line);
     stop_getline(&getline);
     return 0;
